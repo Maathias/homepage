da9ea7d1386f89094661134af6e632350c58d08c
Mateusz Pstrucha <pstrucha.mateusz@gmail.com>
2020-11-19 11:12:54 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,38 @@
+const Panes = {
+  active: null,
+  switch(target) {
+    if (target == this.active) return
+    this.active = target
+    document.querySelector('.butt.active').classList.remove('active')
+    document.querySelector(`.butt[data-target=${target}]`).classList.add('active')
+  }
+}
+
+window.onload = function () {
+  document.querySelector('#nav').onclick = function (e) {
+    let target = e.target.getAttribute('data-target')
+    if (target) {
+      // location.href = `#${target}`
+      clearTimeout(scrollTimeout)
+      document.querySelector(`#${target}`).scrollIntoView({
+        behavior: 'smooth'
+      })
+      Panes.switch(target)
+    }
+  }
+
+  var scrollTimeout,
+    wrapper = document.querySelector('.wrapper')
+
+  wrapper.addEventListener('scroll', function (e) {
+    let x = wrapper.scrollTop / window.innerHeight,
+      y = Math.floor(x + .3)
+
+    clearTimeout(scrollTimeout)
+    scrollTimeout = setTimeout(function () {
+      Panes.switch(['about', 'contact', 'more'][y])
+    }, 75)
+
+  });
+
+}
